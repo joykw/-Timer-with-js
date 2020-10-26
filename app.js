f28e339e046da129d7460322c8fce701813388d5
joykw <wanjikujoy@rocketmail.com>
2020-10-26 16:39:33 +0000

    Basic clock with reset button functioning completed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Data and Variables
 let timer;
+let oldTime;
 const SECONDS_IN_MINUTES = 60;
-let timeLeft = 24 * SECONDS_IN_MINUTES + 7; //Minutes as seconds plus the second
+let timeLeft = 25 * SECONDS_IN_MINUTES + 00; //Minutes as seconds plus the second
 
 //Cache the DOM
 const display = document.querySelector('.display');
@@ -10,24 +11,47 @@ const seconds = display.querySelector('.seconds');
 const controls = document.querySelector('.controls');
 const starttButton = controls.querySelector('.start');
 const stopButton = controls.querySelector('.stop');
+const resetButton = controls.querySelector('.reset');
 
 
 //Add listeners
 starttButton.addEventListener('click', start);
 stopButton.addEventListener('click', stop);
+resetButton.addEventListener('click', reset);
 
 //Initialization
 render();
     
     //Functions
 function start(){
-    timer = setInterval(tick, 1000);
+    oldTime = timeLeft;
+    if(!timer){
+        timer = setInterval(tick, 1000);
+    }  
 }
+
+
 function tick(){
     timeLeft -= 1
     render();
 }
-function stop (){}
+
+
+function stop (){
+    clearInterval(timer);
+    timer = undefined;
+}
+
+function reset(){
+    if(!oldTime){
+        return;
+    }
+    timeLeft = oldTime;
+    stop();
+    render();
+
+}
+
 
 function render(){
 const minutesLeft = Math.floor(timeLeft / SECONDS_IN_MINUTES);
